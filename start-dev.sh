45ed2afe97a3af7c26e0c3eba7f214b6ac5cd1a7
Felix Schüßler <f.schuessler@Mac.fritz.box>
2026-05-18 13:24:18 +0200

    Remove local dev files from repo

--- start-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/Users/f.schuessler/.nvm/versions/node/v24.15.0/bin:$PATH"
-cd /Users/f.schuessler/kochelino-app
-exec node node_modules/.bin/next dev --port 3002 --webpack
